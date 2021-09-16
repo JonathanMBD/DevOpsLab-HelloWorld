f22314cf29cd29139dd655a74c416b23742de52b
Jonathan <jonathan@mbd.com.br>
2021-09-16 16:12:41 -0300

    Teste andre 2

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Pipeline DevOps JOnathan de Paula")
+        self.assertRegex(result.data.decode(), "Pipeline DevOps Andre Baitola")
 
 
 if __name__ == "__main__":
